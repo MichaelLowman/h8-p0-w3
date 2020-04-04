b4aef00fb96a617d1909abb725e1c8be1caafbf2
MichaelLowman <lowmanm679@gmail.com>
2020-04-04 14:17:26 +0700

    tugas week 3 final

--- jumat/exercise-15.js
+++ jumat/exercise-15.js
@@ -1,32 +1,65 @@
 function groupAnimals(animals) {
     // you can only write your code here!
-   var newAnimal= [];
-   
-   for(let i=0; i<animals.length; i++){
-       for(let j=0; j<animals.length; j++){
-          if(animals[j] > animals[j+1]){
-              var temp = animals[j+1]
-              animals[j+1] = animals[j]
-            animals[j] = temp;
-          }
-          
-         
+   let newAnimals = [];
+   let binatang = []
+   var hurufa = []
+   var str = ''
+   var huruf =''
+   var kebunbinatang=[];
 
-       }
-   }
-   
+
    for(i=0; i<animals.length; i++){
-       newAnimal =[]
-       for(j=0; j<animals.length; j++){
-           if(animals[j][0] == animals[j][0]){
+       huruf += animals[i][0];
+       
+} //console.log(huruf)
 
-               newAnimal.push(animals[i])
+         for(let i=0; i<huruf.length; i++ ){
+             var flag = false
+            for(let j=0; j<huruf.length; j++){
+
+           if(huruf[i] == hurufa[j]){
+               flag = true;}    
            }
-         
-       }
-   }
+           
+           if(flag == false){
+           hurufa.push(huruf[i]);   
+           }
 
-    return newAnimal;
+       }
+  
+        
+       for(i=0; i<hurufa.length; i++){
+        binatang = []
+        for(j=0; j<animals.length; j++){
+            if(hurufa[i] == animals[j][0]){
+
+                binatang.push(animals[j])
+                
+            }
+        }
+        kebunbinatang.push(binatang)
+        }
+     
+
+       for(let i=0; i<kebunbinatang.length; i++ ){
+        for(let j=0; j<kebunbinatang.length; j++){
+
+            if(kebunbinatang[j]>kebunbinatang[j+1]){
+
+                var y = kebunbinatang[j+1]
+                kebunbinatang[j+1] = kebunbinatang[j]
+                kebunbinatang[j] = y
+
+            }
+            
+        }
+    }
+
+
+                    
+
+return kebunbinatang
+
 
   }
   
